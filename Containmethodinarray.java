53dab9075c90db6e74b36ea708d95cdda8d55260
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-12 18:41:33 +0530

    Add files via upload

---
+++ Containmethodinarray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Containmethodinarray{
+    public static void main(String args[]){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(989);
+        a.add(89);
+        a.add(908);
+        a.add(900);
+        System.out.println(a.contains(90));
+        System.out.println(a.contains(900)); 
+    }
+}
