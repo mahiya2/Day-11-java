12015c41d36dcc96396e872b6b2334c850dc58bd
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-12 18:31:40 +0530

    Add files via upload

---
+++ Arraylistsize.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Arraylistsize{
+    public static void main(String args[]){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(989);
+        a.add(89);
+        a.add(1);
+        a.add(78);
+        System.out.println(a.size());
+    }
+}
