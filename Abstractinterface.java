081f762a27f91831eba1b331484f36884803b0d4
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-12 11:41:35 +0530

    Add files via upload

---
+++ Abstractinterface.java
@@ -0,0 +1,15 @@
+abstract interface math{
+    void print();
+}
+class d implements math{
+    //@overriding
+    public void print(){
+        System.out.println("hello");
+    }
+}
+public class Abstractinterface{
+    public static void main(String args[]){
+        d f=new d();
+        f.print();
+    }
+}
